61902a6eb8b58c41d8d1c34c5eeea913a1a95068
Benoit Brochier <benoit.brochier@gmail.com>
2020-12-12 09:23:27 +0100

    save

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -5,7 +5,7 @@ class Admin extends Component {
   render() {
     return (
       <div className="cards">
-        <AjouterRecette AjouterRecette={this.props.ajouterRecette} />
+        <AjouterRecette ajouterRecette={this.props.ajouterRecette} />
         <footer>
           <button onClick={this.props.chargerExemple}>Remplir</button>
         </footer>
